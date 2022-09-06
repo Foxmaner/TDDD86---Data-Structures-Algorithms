b2f9121f4350e52897ebaefe96b3704d1e970e11
eskbr129 <eskbr129@su25-311.ad.liu.se>
2022-09-06 14:26:11 +0200

    TDDD86 Lab 1 redovisning

--- lab1/life/src/life.cpp
+++ lab1/life/src/life.cpp
@@ -1,6 +1,17 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
+/*
+ * Programmerare: Eskbr129, Bjoed735
+ * Eskil Brännerud, Björn Edblom
+ * Vår labb var att skapa spelet Life,
+ * det går utt på att bakterier kommer växa
+ * och dö beroende på hur många granar den har.
+ * En position som har noll eller en granne kommer att vara tom i nasta generation. Om en cell fanns där så dör den¨
+ * En position med tva grannar ˚ ar stabil. Om den hade en cell inneh ¨ aller den fortfarande en cell. Om den
+ * var tom fortsatter den att vara tom. ¨
+ * En position med tre grannar kommer att innehalla en cell i n ˚ asta generation. Om positionen var tom föds ¨
+ * en ny cell. Om den for närvarande innehåller en cell, kvarststår cellen. ˚
+ * En position med fyra eller fler grannar kommer att vara tom i nasta generation. Om det finns en cell i ¨
+ * den positionen dor den av ¨ overbefolkning.
+ */
 
 #include <iostream>
 #include "grid.h"
@@ -10,6 +21,16 @@
 
 using namespace std;
 
+/**
+ * Prints grid
+ *
+ * This function prints the whole grid
+ * - is an empty cell
+ * and x is an occupied cell
+ *
+ * @param grid
+ * @return n/a
+ */
 void printGrid(const Grid<int>& grid){
     for(int row=0;row<grid.numRows();row++){
         for(int col=0;col<grid.numCols();col++){
@@ -25,11 +46,19 @@ void printGrid(const Grid<int>& grid){
 }
 
 
-
+/**
+ * Return number of neighbors
+ *
+ * This function returns the number of neighbors
+ * for a specific position
+ *
+ * @param grid,row,column
+ * @return nrOfNeighbor
+ */
 int numberOfNeighbor(const Grid<int>& grid, int row, int colum){
     int numberOfNeighbor = 0;
 
-    // Top row
+    // Checks neibor on top row
     if(grid.inBounds(row-1,colum-1)){
         if(grid[row - 1][colum - 1] == 1){
             numberOfNeighbor ++;
@@ -47,8 +76,7 @@ int numberOfNeighbor(const Grid<int>& grid, int row, int colum){
     }
 
 
-    // Middle row without itself
-
+    // Check middle row without itself
     if(grid.inBounds(row,colum - 1)) {
         if(grid[row][colum - 1] == 1){
             numberOfNeighbor ++;
@@ -60,8 +88,7 @@ int numberOfNeighbor(const Grid<int>& grid, int row, int colum){
         }
     }
 
-    // Bottom row
-
+    // Check bottom row
     if(grid.inBounds(row+1,colum - 1)) {
         if(grid[row + 1][colum - 1] == 1){
             numberOfNeighbor ++;
@@ -81,26 +108,39 @@ int numberOfNeighbor(const Grid<int>& grid, int row, int colum){
 
     return numberOfNeighbor;
 }
+/**
+ * Ticks the game one step forward
+ *
+ * This function ticks the game one step forward
+ * according to the game rules. It creates a new grid which is then
+ * returned to be later used
+ *
+ * @param grid,nrOfRows,nrOfColumns
+ * @return new grid
+ */
 
- Grid<int> tick( Grid<int>& grid,int nrOfRows, int nrOfColumns){
+ Grid<int> tick(const Grid<int>& grid,int nrOfRows, int nrOfColumns){
+     // Create a new grid
     Grid<int> newGrid = Grid<int>(nrOfRows,nrOfColumns);
+    // Goes thorugh every tile to check the numbers of neighbors
+    // and add/remove cells accordingly
     for(int row = 0; row <nrOfRows;row ++ ){
         for(int colum = 0; colum<nrOfColumns; colum ++){
             int count = numberOfNeighbor(grid,row,colum);
-
+            // If one neighbor the cell dies
             if(count <= 1){
                 newGrid.set(row,colum,0);
             }
-
+            // If three neighbors the cell filled
             else if(count == 3){
                 newGrid.set(row,colum,1);
             }
-
+            // If two neighbors keep the previous value
             else if(count == 2){
                 // Check start value
                 newGrid.set(row,colum,grid[row][colum]);
             }
-
+            // If three or more the cell dies
             else if(count >= 3){
                 newGrid.set(row,colum,0);
             }
@@ -109,7 +149,16 @@ int numberOfNeighbor(const Grid<int>& grid, int row, int colum){
     return newGrid;
 }
 
-
+ /**
+  * Main function
+  *
+  * This function is the main function
+  * It initilizes the the game, and then
+  * handle user imputs to determine what to do.
+  *
+  * @param n/a
+  * @return n/a
+  */
 int main() {
     //Filename for initGame. max 20 long
     string fileName;
@@ -143,6 +192,8 @@ int main() {
 
     string nextLine;
 
+    // Fills the empty game board with occupied cells
+    // according to the text file
     for(int i=0;i<nrOfRows;i++){
         MyReadFile >> nextLine;
         for(int j=0;j<nrOfColumns;j++){
@@ -174,7 +225,8 @@ int main() {
                 grid = tick(grid,nrOfRows,nrOfColumns);
                 cout << flush;
                 printGrid(grid);
-                pause(1000);
+                //pauses program for 100ms
+                pause(100);
             }
         }else{
             cout << "Try again!\n";
