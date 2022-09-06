c2e4488cabe4fd962088467c3e41f0a2247cc2ff
eskbr129 <eskbr129@tlvm-4-1-4.ad.liu.se>
2022-09-06 19:29:32 +0200

    some pseudo to code

--- lab2/a_del/wordchain/src/wordchain.cpp
+++ lab2/a_del/wordchain/src/wordchain.cpp
@@ -7,6 +7,34 @@ using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
+void wordChain(string firstWord, string lastWord){
+    queue<stack<string>> stackQueue;
+
+    stack<string> firstWordStack;
+    firstWordStack.push(firstWord);
+
+    stackQueue.push(firstWordStack);
+
+    while(stackQueue.size()){
+        stack<string> currentStack = stackQueue.front();
+        stackQueue.pop();
+
+        string wordAtTop = currentStack.top();
+
+        if(wordAtTop==lastWord){
+            cout << "Hitta ordet. listan kommer.... \n";
+            while(!currentStack.empty()){
+                string partWord = currentStack.top();
+                std::cout << partWord;
+                currentStack.pop();
+            }
+        }else{
+
+        }
+
+    }
+}
+
 int main() {
     cout << "Welcome to TDDD86 Word Chain." << endl;
     cout << "If you give me two English words, I will transform the" << endl;
@@ -18,6 +46,8 @@ int main() {
     // Takes in the first and last word.
     string firstWord,lastWord;
     cin >> firstWord >> lastWord;
+
+    wordChain(firstWord,lastWord);
 
     return 0;
 }
