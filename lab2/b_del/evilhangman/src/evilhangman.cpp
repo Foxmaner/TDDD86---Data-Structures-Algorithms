f00acb9e84bb8c8c1361de49ca68540bc4fce88b
eskbr129 <eskbr129@tlvm-4-4-4.ad.liu.se>
2022-09-12 12:48:01 +0200

    now creates a map from dict

--- lab2/b_del/evilhangman/src/evilhangman.cpp
+++ lab2/b_del/evilhangman/src/evilhangman.cpp
@@ -1,13 +1,53 @@
 #include <iostream>
 #include <string>
+#include <set>
+#include <fstream>
+#include <map>
 using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
+
+/*
+Creates a map of sets with strings.
+It groups strings together with words of same length.
+*/
+map<int, set<string>> loadFile(){
+    map<int, set<string>> dictionary;
+    //Dictionary
+    ifstream MyReadFile("dictionary.txt");
+    string currentWord;
+    // While there are more words to be read, keep pushing into vektor
+
+    //Read new words.
+    while (MyReadFile >> currentWord){
+        if(currentWord.length() == 0){
+            continue;
+        }else if(!dictionary.count(currentWord.length())){
+            //Key does not already exist, creates a new one
+            cout<<currentWord + "\n";
+            set<string> tempSet;
+            tempSet.insert(currentWord);
+            dictionary.insert(pair<int, set<string>>(currentWord.length(), tempSet));
+        }else{
+            //Key already exist. Insert at correct key.
+            dictionary.at(currentWord.size()).insert(currentWord);
+        }
+    }
+    return dictionary;
+}
+
 
 int main() {
     cout << "Welcome to Hangman." << endl;
 
     // TODO: Finish the program!
+    map<int, set<string>> myMap = loadFile();
 
+
+    /*
+    set<string> si = myMap.at(3);
+    for(std::set<string>::iterator it=si.begin(); it!=si.end(); ++it)
+        std::cout << *it << std::endl;
+    */
     return 0;
 }
