5cd1cf0cb8266b2ed15c77dcbd6d043ff5f4c199
Björn Edblom <bjoed735@student.liu.se>
2022-11-30 14:08:33 +0100

    klar med myvector

--- lab6/MyVector.h
+++ lab6/MyVector.h
@@ -77,7 +77,6 @@ MyVector<T>::MyVector(const MyVector& other){
     for (unsigned int i= 0; i < other.capacity; i ++){
         storage[i] = other.storage[i];
     }
-    return this;
 }
 
 /*
@@ -93,65 +92,107 @@ MyVector<T>& MyVector<T>::operator =(const MyVector& other){
     capacity = other.capacity;
     numberOfElements = other.numberOfElements;
     storage = new T[other.capacity];
-    for (unsigned int i= 0; i < other.capacity; i ++){
+    for (unsigned int i= 0; i < other.capacity; i++){
         storage[i] = other.storage[i];
     }
 
     return this;
 }
 
+
+/*
+ * Add a element to the list if the list is full double the size and then add the element
+*/
+
 template<typename T>
 void MyVector<T>::push_back(const T& e){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    if(numberOfElements == capacity){
+        capacity = capacity * 2;
+        T* templist = new T[capacity];
+        for (unsigned int i= 0; i < numberOfElements; i++){
+            templist[i] = storage[i];
+        }
+        delete [] storage;
+        storage = templist;
+    }
+    storage[numberOfElements] = e;
+    numberOfElements++;
 }
 
+/*
+ * Delete the last element
+*/
 template<typename T>
 void MyVector<T>::pop_back(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    delete storage[numberOfElements];
+    numberOfElements--;
+
 }
 
+
+/*
+ * Return element at a given position
+*/
 template<typename T>
 T& MyVector<T>::operator[](unsigned i){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return storage[i];
 }
 
+/*
+ * Return element at a given position
+*/
 template<typename T>
 const T& MyVector<T>::operator[](unsigned i)const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return storage[i];
 }
 
+
+/*
+ * Check if the list is empty or not
+*/
 template<typename T>
 bool MyVector<T>::empty()const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    if(numberOfElements == 0){
+        return true;
+    }else{
+        return false;
+    }
 }
 
+/*
+ * Delete the list and create a new one with capacity 1
+*/
 template<typename T>
 void MyVector<T>::clear(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    delete [] storage;
+    capacity = 1;
+    numberOfElements = 0;
+    storage = new T[1];
 }
 
+
+/*
+ * Return the size of the list
+*/
 template<typename T>
 unsigned MyVector<T>::size()const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return  numberOfElements;
 }
 
+/*
+ * Return the first element of the list
+*/
 template<typename T>
 T* MyVector<T>::begin(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return &storage[0];
 }
 
+/*
+ * Return the last element of the list
+*/
 template<typename T>
 T* MyVector<T>::end(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return &storage[numberOfElements];
 }
 
 #endif // MY_VECTOR_H
