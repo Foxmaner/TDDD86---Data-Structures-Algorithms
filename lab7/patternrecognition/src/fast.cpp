e94d62d5e86aa51b7e0b96ff5e98423d79d9da5e
eskbr129 <eskbr129@tlvm-4-4-4.ad.liu.se>
2022-12-07 21:22:33 +0100

    sum stuff

---
+++ lab7/patternrecognition/src/fast.cpp
@@ -0,0 +1,106 @@
+/*
+ * TDDD86 Pattern Recognition
+ * This program computes and plots all line segments involving 4 points
+ * in a file using Qt.
+ */
+
+#include <QApplication>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <chrono>
+#include "Point.h"
+
+// constants
+static const int SCENE_WIDTH = 512;
+static const int SCENE_HEIGHT = 512;
+
+void render_points(QGraphicsScene* scene, const vector<Point>& points) {
+    for(const auto& point : points) {
+        point.draw(scene);
+    }
+}
+
+void render_line(QGraphicsScene* scene, const Point& p1, const Point& p2) {
+    p1.lineTo(scene, p2);
+}
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    // open file
+    string filename = "input100.txt";
+    ifstream input;
+    input.open(filename);
+
+    // the vector of points
+    vector<Point> points;
+
+    // read points from file
+    int N;
+    int x;
+    int y;
+
+    input >> N;
+
+    for (int i = 0; i < N; ++i) {
+        input >> x >> y;
+        points.push_back(Point(x, y));
+    }
+    input.close();
+
+    // setup graphical window
+    QGraphicsView *view = new QGraphicsView();
+    QGraphicsScene *scene = new QGraphicsScene(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+    view->setScene(scene);
+    // draw points to screen all at once
+    render_points(scene, points);
+    view->scale(1, -1); //screen y-axis is inverted
+    view->resize(view->sizeHint());
+    view->setWindowTitle("Brute Force Pattern Recognition");
+    view->show();
+
+    // sort points by natural order
+    // makes finding endpoints of line segments easy
+    sort(points.begin(), points.end());
+    auto begin = chrono::high_resolution_clock::now();
+
+    //
+    //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    //Here we implement the new algorithm!!!
+    //
+
+    //Go through every point
+    for (size_t p = 0; p < points.size(); ++p) {
+        //A map with slopes as key, and a vector of points
+        map<double , vector<Point>> slopeList;
+        //Go through every other point.
+        for(size_t p1 = p+1; p1 < points.size(); p1++){
+            //Add the point to the array at the key with corresponding slope.
+            slopeList[points.at(p).slopeTo(points.at(p1))].push_back(points.at(p1));
+        }
+
+        for(const auto &pair : slopeList){
+            if(pair.second.size()<=3){
+                //Draw line form first point to last point
+                render_line(scene, points.at(p), *(pair.second.end() - 1));
+                a.processEvents();
+            }
+        }
+
+    }
+
+    //
+    //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    //
+
+    auto end = chrono::high_resolution_clock::now();
+    cout << "Computing line segments took "
+         << std::chrono::duration_cast<chrono::milliseconds>(end - begin).count()
+         << " milliseconds." << endl;
+
+    return a.exec(); // start Qt event loop
+}
