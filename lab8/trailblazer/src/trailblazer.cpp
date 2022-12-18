72c5bfeabb827b187f4c418ce6c83156a78bf68d
Björn Edblom <bjoed735@student.liu.se>
2022-12-18 17:48:18 +0100

    ska ha fixat git problemen

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -8,12 +8,36 @@
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
+bool depthFirstReq(BasicGraph& graph, Vertex* start, Vertex* end, vector<Node*>& path){
+    start->visited = true;
+    start->setColor(GREEN);
+    // If you have reached the end.
+    if(start == end){
+        path.push_back(start);
+        return true;
+    }
+    else{
+        // Check every neighbor and see if they are not visited
+        for(auto n: graph.getNeighbors(start)){
+            if(!n->visited){
+                if(depthFirstReq(graph,n, end , path)){
+                    path.push_back(start);
+                    return true;
+                }
+            }
+        }
+    }
+    start->setColor(GRAY);
+    return false;
+
+}
+
+
+
 vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
     vector<Vertex*> path;
+    graph.resetData();
+    depthFirstReq(graph, start, end, path);
     return path;
 }
 
